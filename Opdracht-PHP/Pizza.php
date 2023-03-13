5af7bd6f3fa70e749bfb092061cf2f3c94fb7321
Govert Jansen <97050782@st.deltion.nl>
2023-03-13 20:32:10 +0100

    Alles werkend

--- Opdracht-PHP/Pizza.php
+++ Opdracht-PHP/Pizza.php
@@ -68,9 +68,9 @@
                     <input id="date" type="datetime-local" name="tijd" class="tekstinput" required>
 
                     <select name="keuze">
-                        <option disabled selected hidden value="">Maak uw keuze:</option>
-                        <option value="Afhalen">Afhalen</option>
-                        <option value="Bezorgen">Bezorgen</option>
+                        <option name="afbe" disabled selected hidden value="">Maak uw keuze:</option>
+                        <option name="afbe" value="Afhalen">Afhalen</option>
+                        <option name="afbe" value="Bezorgen">Bezorgen</option>
                     </select>
 
                     <input class="bestellen" type="submit" value="Bestellen" name="submit">
@@ -80,31 +80,33 @@
 
 
             <table class="tabel1">
+                <h5>Maandag alle pizza's 7.50,-<h5>
+                        <h5>Vrijdag 15% korting op je bestelling vanaf 20,-<h5>
+                                <h5>Bezorg kosten bedragen 5,-<h5>
+                                        <tr>
+                                            <td class="tabel">Pizza Margherita</td>
+                                            <td class="tabel">12,50</td>
+                                            <td class="tabel"><input type="number" name="AantalMargherita" size="3" min="0" value="0"></td>
+                                        <tr>
+                                        <tr>
+                                            <td class="tabel">Pizza Funghi</td>
+                                            <td class="tabel"> 12,50</td>
+                                            <td class="tabel"><input type="number" name="AantalFunghi" size="3" min="0" value="0"></td>
+                                        <tr>
+                                        <tr>
+                                            <td class="tabel">Pizza Marina</td>
+                                            <td class="tabel">13,95</td>
+                                            <td class="tabel"><input type="number" name="AantalMarina" size="3" min="0" value="0"></td>
+                                        <tr>
+                                            <td class="tabel">Pizza Hawai</td>
+                                            <td class="tabel">11,50</td>
+                                            <td class="tabel"><input type="number" name="AantalHawai" size="3" min="0" value="0"></td>
 
-                <tr>
-                    <td class="tabel">Pizza Margherita</td>
-                    <td class="tabel">12,50</td>
-                    <td class="tabel"><input type="number" name="AantalMargherita" size="3" min="0" value="0"></td>
-                <tr>
-                <tr>
-                    <td class="tabel">Pizza Funghi</td>
-                    <td class="tabel"> 12,50</td>
-                    <td class="tabel"><input type="number" name="AantalFunghi" size="3" min="0" value="0"></td>
-                <tr>
-                <tr>
-                    <td class="tabel">Pizza Marina</td>
-                    <td class="tabel">13,95</td>
-                    <td class="tabel"><input type="number" name="AantalMarina" size="3" min="0" value="0"></td>
-                <tr>
-                    <td class="tabel">Pizza Hawai</td>
-                    <td class="tabel">11,50</td>
-                    <td class="tabel"><input type="number" name="AantalHawai" size="3" min="0" value="0"></td>
-
-                <tr>
-                    <td class="tabel">Pizza Quattro Formaggi</td>
-                    <td class="tabel">14,50</td>
-                    <td class="tabel"><input type="number" name="AantalFormaggi" size="3" min="0" value="0"></td>
-                </tr>
+                                        <tr>
+                                            <td class="tabel">Pizza Quattro Formaggi</td>
+                                            <td class="tabel">14,50</td>
+                                            <td class="tabel"><input type="number" name="AantalFormaggi" size="3" min="0" value="0"></td>
+                                        </tr>
             </table>
 
             <!-- <table class="tabel1">
@@ -126,13 +128,48 @@
 
         </form>
 
-
         <?php if (isset($_POST["submit"])) {
             $AantalMargherita = $_POST["AantalMargherita"];
             $AantalFunghi = $_POST["AantalFunghi"];
             $AantalMarina = $_POST["AantalMarina"];
             $AantalHawai = $_POST["AantalHawai"];
             $AantalFormaggi = $_POST["AantalFormaggi"];
+
+            if ($AantalMargherita > 0 || $AantalFunghi > 0 || $AantalMarina > 0 || $AantalHawai > 0 || $AantalFormaggi > 0) {
+                if (date('D', $datum) == "Mon") {
+                    $PrijsMargherita = $AantalMargherita * 7.50;
+                    $PrijsFunghi = $AantalFunghi * 7.50;
+                    $PrijsMarina = $AantalMarina * 7.50;
+                    $PrijsHawai = $AantalHawai * 7.50;
+                    $PrijsFormaggi = $AantalFormaggi * 7.50;
+                    $eindtotaal = $PrijsMargherita + $PrijsFunghi + $PrijsMarina + $PrijsHawai + $PrijsFormaggi;
+                } elseif (date("D", $datum) == "Fri") {
+                    $PrijsMargherita = $AantalMargherita * 12.50;
+                    $PrijsFunghi = $AantalFunghi * 12.50;
+                    $PrijsMarina = $AantalMarina * 13.95;
+                    $PrijsHawai = $AantalHawai * 11.50;
+                    $PrijsFormaggi = $AantalFormaggi * 14.50;
+                    $eindtotaal = $PrijsMargherita + $PrijsFunghi + $PrijsMarina + $PrijsHawai + $PrijsFormaggi;
+                    if ($eindtotaal >= 20) {
+                        $eindtotaal = $eindtotaal * 0.80;
+                    }
+                } else {
+                    $PrijsMargherita = $AantalMargherita * 12.50;
+                    $PrijsFunghi = $AantalFunghi * 12.50;
+                    $PrijsMarina = $AantalMarina * 13.95;
+                    $PrijsHawai = $AantalHawai * 11.50;
+                    $PrijsFormaggi = $AantalFormaggi * 14.50;
+                    $eindtotaal = $PrijsMargherita + $PrijsFunghi + $PrijsMarina + $PrijsHawai + $PrijsFormaggi;
+                }
+                if ($_POST["keuze"]) {
+                    $keuze = $_POST["keuze"];
+                    if ($keuze == "Bezorgen") {
+                        $eindtotaal = $eindtotaal + 5;
+                    } else {
+                        $eindtotaal;
+                    }
+                }
+            }
 
             echo ' <table class="tabel2">
          <tr>
@@ -145,35 +182,52 @@
              <td class="tabel"> ';
             echo $AantalMargherita;
             echo  ' </td>
-             <td class="tabel"> </td>
+             <td class="tabel"> ';
+            echo $PrijsMargherita;
+            echo  ' </td>
          </tr>
          <tr>
              <td class="tabel">Pizza Funghi</td>
              <td class="tabel"> ';
             echo $AantalFunghi;
             echo  ' </td>
-             <td class="tabel"> </td>
+             <td class="tabel"> ';
+            echo $PrijsFunghi;
+            echo ' </td>
          </tr>
          <tr>
              <td class="tabel">Pizza Marina</td>
              <td class="tabel"> ';
             echo $AantalMarina;
             echo ' </td>
-             <td class="tabel"> </td>
+             <td class="tabel"> ';
+            echo $PrijsMarina;
+            echo ' </td>
          </tr>
+         <tr>
              <td class="tabel">Pizza Hawai</td>
              <td class="tabel"> ';
             echo $AantalHawai;
             echo  ' </td>
-             <td class="tabel"> </td>
-
+             <td class="tabel"> ';
+            echo $PrijsHawai;
+            echo ' </td>
+        </tr>
          <tr>
              <td class="tabel">Pizza Quattro Formaggi</td>
              <td class="tabel"> ';
             echo $AantalFormaggi;
             echo  ' </td>
-             <td class="tabel"> </td>
+             <td class="tabel"> ';
+            echo $PrijsFormaggi;
+            echo ' </td>
          </tr> 
+            
+         <tr>
+             <td class="tabel">Totale prijs: ';
+            echo $eindtotaal;
+            echo ' </td>
+        </tr>
 
      </table> ';
         }
