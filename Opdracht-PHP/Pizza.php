d1f6a046b361b90ea2070fa1938d9538e3a7b133
Govert Jansen <97050782@st.deltion.nl>
2023-03-23 20:56:14 +0100

    Voor de zekerheid --> Bon veranderingen

--- Opdracht-PHP/Pizza.php
+++ Opdracht-PHP/Pizza.php
@@ -300,55 +300,61 @@ $eindtotaal = "0";
             </tr> ';
             }
 
-            if ($AantalMargherita > 0 || $AantalFunghi > 0 || $AantalMarina > 0 || $AantalHawai > 0 || $AantalFormaggi > 0) {
-                $EindtotaalVoorKorting = ($PrijsMargherita * $AantalMargherita) + ($PrijsFunghi * $AantalFunghi) + ($PrijsMarina * $AantalMarina) + ($PizzaHawai * $AantalHawai) + ($PizzaQuattroFormaggi * $AantalFormaggi);
-                echo ' <tr>
-             <td class="tabel">Totaal:</td>';
-                echo ' <td class="tabel"> ';
-                echo '€' . number_format("$EindtotaalVoorKorting", 2, ",");
-                echo ' </td>
-            </tr> ';
-            };
 
 
             if (date("D", $datum) == "Fri") {
-                $Korting = $EindtotaalVoorKorting - $eindtotaal;
-                echo ' <tr>
-             <td class="tabel">Korting:</td> ';
-                echo  ' <td class="tabel"> ';
-                echo '€' . number_format("$Korting ", 2, ",");
-                echo ' </td> 
-            </tr> ';
-            };
-
-            if ($_POST["keuze"]) {
-                $keuze = $_POST["keuze"];
-                if ($keuze == "Bezorgen") {
-                    $eindtotaal = $eindtotaal + $BezorgKosten;
-                } else {
-                    $eindtotaal;
-                }
-            }
-
-            if ($AantalMargherita > 0 || $AantalFunghi > 0 || $AantalMarina > 0 || $AantalHawai > 0 || $AantalFormaggi > 0) {
-                if ($keuze == "Bezorgen") {
+                if ($AantalMargherita > 0 || $AantalFunghi > 0 || $AantalMarina > 0 || $AantalHawai > 0 || $AantalFormaggi > 0) {
+                    $EindTotaalVoorKorting = ($PrijsMargherita * $AantalMargherita) + ($PrijsFunghi * $AantalFunghi) + ($PrijsMarina * $AantalMarina) + ($PizzaHawai * $AantalHawai) + ($PizzaQuattroFormaggi * $AantalFormaggi);
                     echo ' <tr>
-                      <td class="tabel">Bezorgkosten </td> ';
+                    <td class="tabel">Totaal:</td>';
                     echo ' <td class="tabel"> ';
-                    echo '€' . number_format("$BezorgKosten", 2, ",");
-                    ' </td>
-            </tr> ';
-                };
+                    echo '€' . number_format("$EindTotaalVoorKorting", 2, ",");
+                    echo ' </td>
+                    </tr> ';
+                }
             };
 
-            if ($AantalMargherita > 0 || $AantalFunghi > 0 || $AantalMarina > 0 || $AantalHawai > 0 || $AantalFormaggi > 0) {
-                echo ' <tr>
-             <td class="tabel">Te betalen: </td> ';
-                echo '<td class="tabel"> ';
-                echo  '€' . number_format("$eindtotaal", 2, ",");
-                echo ' </td>
-            </tr> ';
-            };
+
+            // if (date("D", $datum) == "Fri") {
+            //     $Korting = $EindTotaalVoorKorting - $eindtotaal;
+            //     echo ' <tr>
+            //  <td class="tabel">Korting:</td> ';
+            //     echo  ' <td class="tabel"> ';
+            //     echo '€' . number_format("$Korting ", 2, ",");
+            //     echo ' </td> 
+            // </tr> ';
+            // };
+
+            // if ($_POST["keuze"]) {
+            //     $keuze = $_POST["keuze"];
+            //     if ($keuze == "Bezorgen") {
+            //         $eindtotaal = $eindtotaal + $BezorgKosten;
+            //     } else {
+            //         $eindtotaal;
+            //     }
+            // }
+
+            // if ($AantalMargherita > 0 || $AantalFunghi > 0 || $AantalMarina > 0 || $AantalHawai > 0 || $AantalFormaggi > 0) {
+            //     if ($keuze == "Bezorgen") {
+            //         echo ' <tr>
+            //           <td class="tabel">Bezorgkosten </td> ';
+            //         echo ' <td class="tabel"> ';
+            //         echo '€' . number_format("$BezorgKosten", 2, ",");
+            //         ' </td>
+            // </tr> ';
+            //     };
+            // };
+
+            // if ($AantalMargherita > 0 || $AantalFunghi > 0 || $AantalMarina > 0 || $AantalHawai > 0 || $AantalFormaggi > 0) {
+            //     echo ' <tr>
+            //  <td class="tabel">Te betalen: </td> ';
+            //     echo '<td class="tabel"> ';
+            //     echo  '€' . number_format("$eindtotaal", 2, ",");
+            //     echo ' </td>
+            // </tr> ';
+            // };
+
+
             ' </table> ';
         }
         ?>
