92d0625be5dc617ee4519c0a621699bf75221fdc
Govert Jansen <97050782@st.deltion.nl>
2023-09-15 16:17:23 +0200

    Laracast episode 19

--- laracast/index.php
+++ laracast/index.php
@@ -1,4 +1,12 @@
 <?php
 
+require 'Database.php';
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+
+
+
+$db = new Database();
+$posts = $db->query("select * from posts")->fetchall(PDO::FETCH_ASSOC);
+
+dd($posts);
